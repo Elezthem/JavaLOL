6cff38d72a37f3a62f69039773b56d72bcab2d55
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-10-29 10:55:34 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = new String("Good Alex");
+        String str2 = "Alex";
+        System.out.println(String.valueOf(65));
+        String str3 = String.valueOf(65);
+        //String.copyValueOf();
+    }
+}
